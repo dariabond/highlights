895970b3c3fbe6cd96372f1da2a250266cf04fcf
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2025-06-02 23:08:54 +0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+@main.route('/api/test', methods=['GET'])
+def test_route():
+    return jsonify({
+        'status': 'success',
+        'message': 'Hello from the Flask test route!'
+    })
+
+@main.route('/', methods=['GET'])
+def index():
+    return jsonify({
+        'status': 'success',
+        'message': 'Welcome to the Highlight API',
+        'test_endpoint': '/api/test'
+    })
